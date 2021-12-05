b2256e42f72442d9f67d2e38620d5af845628c45
Lara <lara.maria.meister@hs-furtwangen.de>
2021-12-05 20:03:04 +0100

    add branches to tree

--- src/treebranches.py
+++ src/treebranches.py
@@ -4,8 +4,49 @@ import bmesh
 import mathutils
 import math
 
+VERTICES = 10
+MIN_SHIFT = 0.9
+MAX_SHIFT = 1.1
 
-def generateTree():
+leavePositions = []
+
+
+def generateBranches(edges, verts, vert, branch, newBranch, lastIndex):
+    for i in range(random.randrange(3, 5)):
+        if branch:
+            branchvert = (vert[0] + random.uniform(0, 1), vert[1] +
+                          random.uniform(0, 1), vert[2] + random.uniform(0, 1))
+        elif branch is False:
+            branchvert = (vert[0] + random.uniform(-1, 0), vert[1] +
+                          random.uniform(-1, 0), vert[2] + random.uniform(0, 1))
+        """ elif branch is 3:
+            branchVert = (vert[0] + random.uniform(-1,0), vert[1] + random.uniform(0,1), vert[2] + random.uniform(0,1))
+        elif branch is 4:
+            branchVert = (vert[0] + random.uniform(0,1), vert[1] + random.uniform(-1,0), vert[2] + random.uniform(0,1)) """
+
+        vert = branchvert
+        verts.append(branchvert)
+        if i is 0:
+            edge = (lastIndex - 1, len(verts) - 1)
+        else:
+            edge = (len(verts) - 2, len(verts) - 1)
+
+        edges.append(edge)
+
+        """  if newBranch and i is not 0:
+             generateBranches(edges, verts, branchvert, not branch, False, len(verts) -1 )
+             newBranch = False """
+    leavePositions.append(branchvert)
+
+def generateLeaves(): 
+    for pos in leavePositions:
+        leaves = bpy.ops.mesh.primitive_ico_sphere_add(radius=random.uniform(1, 1.2), enter_editmode=False, align='WORLD', location=(
+           pos[0], pos[1], pos[2]), scale=(random.uniform(2, 3), random.uniform(2, 3), random.uniform(2, 3)))
+
+   
+
+
+def generateTreeWithBranches():
 
     mesh = bpy.data.meshes.new("tree")  # add the new mesh
     obj = bpy.data.objects.new(mesh.name, mesh)
@@ -13,9 +54,6 @@ def generateTree():
     col.objects.link(obj)
     bpy.context.view_layer.objects.active = obj
 
-    VERTICES = 10
-    MIN_SHIFT = 0.9
-    MAX_SHIFT = 1.1
     verts = []
     edges = []
 
@@ -29,19 +67,21 @@ def generateTree():
             edge = (i-1, i)
             edges.append(edge)
 
+    generateBranches(edges, verts, vert, True, True, VERTICES)
+    generateBranches(edges, verts, vert, False, False, VERTICES)
+
     faces = []
 
     trunk = mesh.from_pydata(verts, edges, faces)
 
-
     me = obj.data
 
-    bpy.ops.object.editmode_toggle()
+    obj.select_set(True)
 
+    bpy.ops.object.mode_set(mode='EDIT')
 
     # Get a BMesh representation
     bm = bmesh.from_edit_mesh(me)
-
 
     verts = bm.verts
     num_verts = len(verts)
@@ -57,7 +97,7 @@ def generateTree():
 
     bmesh.update_edit_mesh(me, True)
 
-    bpy.ops.object.editmode_toggle()
+    bpy.ops.object.mode_set(mode='OBJECT')
 
     skin: bpy.types.SkinModifier = bpy.ops.object.modifier_add(type='SKIN')
 
@@ -74,17 +114,17 @@ def generateTree():
         else:
             rad_x = rad_x - random.uniform(0.02, 0.05)
             rad_y = rad_y - random.uniform(0.025, 0.055)
+        i = i + 1
 
     bpy.ops.object.modifier_add(type='SUBSURF')
     bpy.context.object.modifiers["Subdivision"].render_levels = 1
 
-    leaves = bpy.ops.mesh.primitive_ico_sphere_add(radius=random.uniform(1, 1.2), enter_editmode=False, align='WORLD', location=(
-    posX, posY, posZ + 2), scale=(random.uniform(2.5, 5), random.uniform(2.5, 5), random.uniform(6, 8)))
+    generateLeaves()
 
 
-class Tree(bpy.types.Operator):
+class TreeBranches(bpy.types.Operator):
     """Insert a Tree"""
-    bl_idname = "object.generate_tree"
+    bl_idname = "object.generate_tree_branches"
     bl_label = "Generate a Tree"
 
     @classmethod
@@ -92,9 +132,8 @@ class Tree(bpy.types.Operator):
         return context.active_object is not None
 
     def execute(self, context):
-        generateTree()
+        generateTreeWithBranches()
         return {'FINISHED'}
 
-generateTree()
 
-
+generateTreeWithBranches()
